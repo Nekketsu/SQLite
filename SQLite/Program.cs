442d169c2b771a03da318c123b4d909751005dda
Nekketsu <nekketsu@gmail.com>
2023-05-05 16:40:16 +0200

    Introduction and Setting up the REPL

---
+++ SQLite/Program.cs
@@ -0,0 +1,32 @@
+﻿while (true)
+{
+    PrintPrompt();
+    var input = ReadInput();
+
+    if (input == ".exit")
+    {
+        Environment.Exit(0);
+    }
+    else
+    {
+        Console.WriteLine($"Unrecognized command {input}");
+    }
+}
+
+void PrintPrompt()
+{
+    Console.Write("db > ");
+}
+
+string ReadInput()
+{
+    var input = Console.ReadLine();
+
+    if (input is null)
+    {
+        Console.WriteLine("Error reading input");
+        Environment.Exit(1);
+    }
+
+    return input;
+}
